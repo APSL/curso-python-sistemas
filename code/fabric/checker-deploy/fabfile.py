3dcfa5e2cff16bee6d0cd9baf6b1a1311b24e790
Bernardo Cabezas Serra <bcabezas@apsl.net>
2019-11-28 21:20:09 +0100

    fabric live-coding examples

--- code/fabric/checker-deploy/fabfile.py
+++ code/fabric/checker-deploy/fabfile.py
@@ -8,10 +8,10 @@ import warnings
 
 warnings.filterwarnings(action='ignore',module='.*paramiko.*')
 
-env.hosts = ['52.19.58.78']
+env.hosts = ['curso0']
 #env.port = 2232
 env.user = "ubuntu"
-env.key_filename = "cursopy.pem"
+env.key_filename = "../../cursopy.pem"
 
 
 @task()
@@ -27,6 +27,7 @@ def deps():
 
 @task()
 def build():
+    local("pip install wheel")
     local("pip wheel -w wheels .")
 
 @task()
@@ -38,20 +39,23 @@ def deploy(version="0.0.1"):
      build()
      put("wheels/checkurl-{}-py3-none-any.whl".format(version))
      sudo("pip3 install checkurl-{}-py3-none-any.whl".format(version))
-     version()
 
 # ej: fab config:"http://ono.com","WEB-ono"
 @task()
 def config(url="https://www.apsl.net/", name="WEB-apsl"):
     # Reconfiguramos check-mk NRPE
-    filename = 'crontab'
+
     fab_dir = os.path.dirname(env.real_fabfile)
     template_path = os.path.join(fab_dir, "..",  'templates')
+    
+    
     puts("Aplicando el nuevo mrpe")
-    if not exists("/etc/check-mk"):
-        sudo("mkdir -p /etc/check-mk")
+    if not exists("/etc/check_mk"):
+        sudo("mkdir -p /etc/check_mk")
     env.check_url = url 
     env.check_name = name 
-    upload_template(filename="mrpe.cfg.jinja", destination='/etc/check-mk/mrpe2.cfg', context=env,
+    env.check_warn = 0.3
+
+    upload_template(filename="mrpe.cfg.jinja", destination='/etc/check_mk/mrpe.cfg', context=env,
                     use_sudo=True, backup=False, use_jinja=True, template_dir=template_path)
-    run("cat /etc/check-mk/mrpe2.cfg")
+    run("cat /etc/check-mk/mrpe.cfg")
--- code/fabric/fabfile.py
+++ code/fabric/fabfile.py
@@ -3,9 +3,14 @@ from fabric.context_managers import cd
 from fabric.contrib.files import upload_template, exists
 from fabric.utils import abort, puts
 from fabric.contrib.console import confirm
+import yaml
 
 import warnings
 warnings.filterwarnings(action='ignore',module='.*paramiko.*')
+
+with open("/etc/config.yml", 'r') as f:
+    data = f.read()
+    env.config = yaml.load(data)
 
 env.hosts = ['curso0']
 #env.port = 2232
@@ -15,7 +20,8 @@ env.key_filename = "../../cursopy.pem"
 
 @task()
 def uname():
-    out = run('uname -a')
+    #run("uname -r")
+    out = run('ls /tt')
     puts(out)
 
 # podemos silenciar salida
@@ -30,12 +36,23 @@ def hostname():
 def id():
     sudo('id')
 
+
+@task()
+def prod():
+    env.entorno = "prod"
+    puts("usando entorno prod")
+
+@task()
+def pre():
+    env.entorno = "pre"
+    puts("usando entorno prod")
+
 # Cambiamos hostname al nodo, con parametro
 # ej: fab sethostname:curso3
 @task()
-def sethostname(name="curso1"):
-    sudo("echo 127.0.1.0 {}.localdomain >> /etc/hosts".format(name))
-    sudo('hostnamectl set-hostname {}.localdomain'.format(name))
+def deploy():
+    sudo("echo 127.0.1.0 {}.localdomain >> /etc/hosts".format(env.config["hostname"]))
+    sudo('hostnamectl set-hostname {}.localdomain'.format(env.config["hostname"]))
 
 
 # cd contextmanager
@@ -43,3 +60,5 @@ def sethostname(name="curso1"):
 def lstmp():
     with cd("/tmp"):
         run("ls -l")
+    puts("Homedir:")
+    run("ls -l")
